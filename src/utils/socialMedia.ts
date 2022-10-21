55595f7c0c170e188b7363b7bd0c98b2f403e4e4
Cameron Carley <cam.carley@gmail.com>
2022-10-20 21:36:23 -0400

    Update CSS / general layout

--- src/utils/socialMedia.ts
+++ src/utils/socialMedia.ts
@@ -8,49 +8,34 @@ export type SocialMedia = {
 }
 
 
-let colors = [
-    "#008040", "#9932CC", "#00AA55", "#E00000", "yellow"
-]
-
-let l = colors.length;
-
-const genRandomNum = (): number => Math.floor(Math.random() * (l - 0 + 1)) + 0
-
-const genRandomColor = (): string => colors[genRandomNum()]
-
-
-
-
-
-
 const SocialMediaInfo: Array<SocialMedia> =
     [
         {
-            color: genRandomColor(),
+            color: "#171515",
             href: "https://docs.astro.build/",
             title: "Github",
             body: "My hobby work. Including this website",
             iconName: "github"
         },
         {
-            color: genRandomColor(),
+            color: "#0077B5",
             href: "https://www.linkedin.com/in/cameron-carley-711b07169/?originalSubdomain=ca",
             title: "Linkedin",
             body: "Work stuff! Let's connect!",
             iconName: "linkedin"
         },
         {
-            color: genRandomColor(),
+            color: "#1DA1F2",
             href: "https://www.twitter.com/camcarley",
             title: "Twitter",
             body: "The only social media I bother with",
             iconName: "twitter"
         },
         {
-            color: genRandomColor(),
+            color: "#8a3ab9",
             href: "https://www.instagram.com/walliethegreat/",
             title: "Dogstagram",
-            body: "Keep up with the life of my Great Pyreness, whose either sleeping or causing ruckus",
+            body: "Keep up with the life of my Great Pyreness",
             iconName: "instagram"
         }
     ]
